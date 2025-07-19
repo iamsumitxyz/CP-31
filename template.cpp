9e8f45d7a6cffa6a1743ae9eeaadaed3639c8ae3
Sumit Kumar Sharma <mt1210264@iitd.ac.in>
2025-07-19 15:22:29 +0530

    template added

---
+++ template.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Speed up
+#define fastio() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+// Shortcuts
+#define int long long
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+// Debug (disable before submitting)
+// #ifdef DEBUG
+// #define dbg(x) cerr << #x << " = " << x << endl;
+// #else
+// #define dbg(x)
+// #endif
+
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+// Type aliases
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+
+void solve() {
+    
+    return;
+
+}
+
+// Main
+int32_t main() {
+    fastio();
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+}
